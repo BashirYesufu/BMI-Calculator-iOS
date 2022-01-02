389adac485388c8d2481a4b36f1bf7ab0e2d7aff
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-03 00:11:12 +0100

    Used colour literals to set background colours

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -16,6 +16,7 @@ struct CalculatorBrain {
     mutating func calculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / pow(height, 2)
         bmi = BMI(value: bmiValue, advice: switchAdvice(value: bmiValue), color: switchColors(value: bmiValue))
+       
     }
     
     func getBMIValue() -> String {
@@ -25,11 +26,11 @@ struct CalculatorBrain {
     func switchColors(value: Float) -> UIColor{
         switch value {
         case ...18.4:
-            return .red
+            return #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
         case 18.5...24.8:
-            return .blue
+            return #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         case 24.9...:
-            return .green
+            return #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         default:
             return .white
         }
@@ -48,9 +49,9 @@ struct CalculatorBrain {
         case ...18.4:
             return "Eat more pies!"
         case 18.5...24.8:
-            return "You are doing well"
+            return "You are doing well!"
         case 24.9...:
-            return "You need to watch it"
+            return "You need to watch it!"
         default:
             return "Default"
         }
